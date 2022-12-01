202a30a218af051a330c2b220719fbec8887099c
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-12-01 19:55:49 +0100

    stampa titolo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: { AppTitle },
+  data() {
+    return {
+      messaggio: "Hello Vite"
+    };
+  },
+  methods: {
+    fetchData() {
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <AppTitle></AppTitle>
+  </div>
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ titlePage() }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                title: "La mia prima app con Vite!"
+            }
+        };
+    },
+    methods: {
+        titlePage() {
+            return this.user.title;
+        }
+    }
+};
+</script>
